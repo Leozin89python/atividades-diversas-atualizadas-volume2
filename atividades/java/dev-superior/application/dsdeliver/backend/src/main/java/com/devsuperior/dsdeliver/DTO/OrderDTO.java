aeae662337acae52a3e797ce0def7fff75717526
Leozin89Python <leozin069@gmail.com>
2021-02-01 00:30:05 -0300

    exercicios-java-mysql-workbench-e-outros

--- atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/DTO/OrderDTO.java
+++ atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/DTO/OrderDTO.java
@@ -2,6 +2,9 @@ package com.devsuperior.dsdeliver.DTO;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.devsuperior.dsdeliver.Enum.OrderStatus;
 import com.devsuperior.dsdeliver.entities.Order;
@@ -16,8 +19,20 @@ public class OrderDTO implements Serializable{
 	private Instant moment;
 	private OrderStatus status;
 	
+	private List<ProductDTO> products = new ArrayList<>();
+	
 	public OrderDTO() {
 		
+	}
+	
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
 	}
 
 	public OrderDTO(Order entity) {
@@ -28,10 +43,18 @@ public class OrderDTO implements Serializable{
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
+		products = entity.getProducts()
+				.stream()
+				.map(x -> new ProductDTO(x))
+				.collect(Collectors.toList());
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public List<ProductDTO> getProducts() {
+		return products;
 	}
 
 	public void setId(Long id) {
@@ -76,9 +99,5 @@ public class OrderDTO implements Serializable{
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 }
--- atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,25 @@
 package com.devsuperior.dsdeliver.services;
 
-public class OrderService {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.DTO.OrderDTO;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.repository.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repositoy;
+	
+	@Transactional(readOnly = true)
+	public List <OrderDTO> findAll(){
+		List<Order> list = repositoy.findAll();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
 }
