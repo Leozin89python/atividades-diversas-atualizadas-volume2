9c4d806dd1f3c7bf55b76d989eefea8a8c835a81
Leozin89Python <leozin069@gmail.com>
2021-01-31 23:45:33 -0300

    exercicios-java-mysql-workbench-e-outros

--- atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/DTO/OrderDTO.java
+++ atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/DTO/OrderDTO.java
@@ -1,5 +1,84 @@
 package com.devsuperior.dsdeliver.DTO;
 
-public class OrderDTO {
+import java.io.Serializable;
+import java.time.Instant;
 
+import com.devsuperior.dsdeliver.Enum.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Order;
+
+public class OrderDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String address;
+	private Double latitude;
+	private Double longitude;
+	private Instant moment;
+	private OrderStatus status;
+	
+	public OrderDTO() {
+		
+	}
+
+	public OrderDTO(Order entity) {
+		super();
+		id = entity.getId();
+		address = entity.getAddress();
+		latitude = entity.getLatitude();
+		longitude = entity.getLongitude();
+		moment = entity.getMoment();
+		status = entity.getStatus();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
--- atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
+++ atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -1,6 +1,25 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.DTO.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repository.ProductRepository;
+
+@Service
 public class ProductService {
+
+	@Autowired
+	private ProductRepository repositoy;
 	
-	
+	@Transactional(readOnly = true)
+	public List <ProductDTO> findAll(){
+		List<Product> list = repositoy.findAll();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
 }
