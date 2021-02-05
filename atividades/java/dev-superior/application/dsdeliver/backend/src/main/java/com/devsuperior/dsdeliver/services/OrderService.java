b8e9d1171c4b258aebfec1e1f2875c7dc8015d1e
Leozin89Python <leozin069@gmail.com>
2021-02-05 05:17:19 -0300

    exercicios-java-mysql-workbench-e-outros

--- atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ atividades/java/dev-superior/application/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -20,27 +20,41 @@ import com.devsuperior.dsdeliver.repository.ProductRepository;
 public class OrderService {
 	
 	@Autowired
-	private OrderRepository repositoy;
+	private OrderRepository repository;
 	
 	@Autowired
 	private ProductRepository productRepositoy;
 	
 	@Transactional(readOnly = true)
 	public List <OrderDTO> findAll(){
-		List<Order> list = repositoy.findOrdersWithProducts();
-		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x))
+				.collect(Collectors
+				.toList());
 	}
 	
 	@Transactional
 	public OrderDTO insert(OrderDTO dto) {
-		Order order = new Order(null, dto.getAddress(), dto.getLatitude(),
-				dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		Order order = new Order(null,
+				dto.getAddress(), 
+				dto.getLatitude(),
+				dto.getLongitude(),
+				Instant.now(), 
+				OrderStatus.PENDING);
 		for(ProductDTO p: dto.getProducts()) {
 			Product product = productRepositoy.getOne(p.getId());
 			order.getProducts().add(product);
 		}
 		
-		order = repositoy.save(order);
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
 		return new OrderDTO(order);
 	}
 }
